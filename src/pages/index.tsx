1f4df8f4278752968e0af3fef3b33afc87809227
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-03-17 16:54:18 +0200

    chore(core): connect csr-ssr examples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,42 @@
 /* Core */
-import { NextPage, GetServerSideProps } from 'next';
+import { GetServerSideProps } from 'next';
+import { ApolloError }        from '@apollo/client';
 
 /* Instruments */
 import * as gql       from '@/graphql';
 import { withApollo } from '@/lib';
 
-const IndexPage: NextPage = () => {
+const IndexPage: PageAllProductsComp = () => {
     const userQuery = gql.useUserQuery({ variables: { id: '123' } });
     const userData = JSON.stringify(userQuery.data?.user, null, 4);
 
     return (
         <>
-            <h1>Order Form Prototype</h1>
+            <h1>Order Form Prototype SSR</h1>
             <pre>{userData}</pre>
         </>
     );
 };
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
-    const [ userQuery ] = await Promise.all([
-        gql.ssrUser.getServerPage({ variables: { id: '123' } }, ctx),
-    ]);
+    const userQuery = await gql.getServerPageUser(
+        { variables: { id: '123' } },
+        ctx,
+    );
 
-    return userQuery;
+    return {
+        props: {
+            ...userQuery,
+            TEST: 'TEST',
+        },
+    };
 };
+
+/* Types */
+export type PageAllProductsComp = React.FC<{
+    data?: gql.PageUserComp;
+    error?: ApolloError;
+    TEST: string;
+}>;
 
 export default withApollo(IndexPage);
