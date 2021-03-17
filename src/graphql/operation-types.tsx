ffcfb4adf3c92b773fa7634327bbb942497fbaf7
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2021-03-17 14:12:25 +0200

    chore(deps): bump deps versions

--- src/graphql/operation-types.tsx
+++ src/graphql/operation-types.tsx
@@ -1,112 +1,115 @@
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
-import {
-    FieldPolicy,
-    FieldReadFunction,
-    TypePolicies,
-    TypePolicy,
-} from '@apollo/client/cache';
+import { FieldPolicy, FieldReadFunction, TypePolicies, TypePolicy } from '@apollo/client/cache';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = {
-    [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions = {};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
-    /** The `Upload` scalar type represents a file upload. */
-    Upload: any;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
 };
+
 
 export type Query = {
-    __typename?: 'Query';
-    user: User;
-    session: Scalars['String'];
+  __typename?: 'Query';
+  user: User;
+  session: Scalars['String'];
 };
 
+
 export type QueryUserArgs = {
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 };
 
 export type Mutation = {
-    __typename?: 'Mutation';
-    createUser?: Maybe<User>;
-    startSession: Scalars['Boolean'];
-    endSession: Scalars['Boolean'];
+  __typename?: 'Mutation';
+  createUser?: Maybe<User>;
+  startSession: Scalars['Boolean'];
+  endSession: Scalars['Boolean'];
 };
 
+
 export type MutationCreateUserArgs = {
-    data?: Maybe<UserCreateInput>;
+  data?: Maybe<UserCreateInput>;
 };
 
 export type User = {
-    __typename?: 'User';
-    firstName?: Maybe<Scalars['String']>;
-    lastName?: Maybe<Scalars['String']>;
-    email?: Maybe<Scalars['String']>;
-    password?: Maybe<Scalars['String']>;
+  __typename?: 'User';
+  firstName?: Maybe<Scalars['String']>;
+  lastName?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  password?: Maybe<Scalars['String']>;
 };
 
 export type UserCreateInput = {
-    name: Scalars['String'];
-    email: Scalars['String'];
-    password?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  email: Scalars['String'];
+  password?: Maybe<Scalars['String']>;
 };
 
 export const CacheControlScope = {
-    Public: 'PUBLIC',
-    Private: 'PRIVATE',
+  Public: 'PUBLIC',
+  Private: 'PRIVATE'
 } as const;
 
 export type CacheControlScope = typeof CacheControlScope[keyof typeof CacheControlScope];
 
 export type UserQueryVariables = Exact<{
-    id: Scalars['ID'];
+  id: Scalars['ID'];
 }>;
 
-export type UserQuery = { __typename?: 'Query' } & {
-    user: { __typename?: 'User' } & Pick<
-        User,
-        'firstName' | 'lastName' | 'email' | 'password'
-    >;
-};
 
-export type SessionQueryVariables = Exact<{ [key: string]: never }>;
+export type UserQuery = (
+  { __typename?: 'Query' }
+  & { user: (
+    { __typename?: 'User' }
+    & Pick<User, 'firstName' | 'lastName' | 'email' | 'password'>
+  ) }
+);
 
-export type SessionQuery = { __typename?: 'Query' } & Pick<Query, 'session'>;
+export type SessionQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type LoginMutationVariables = Exact<{ [key: string]: never }>;
 
-export type LoginMutation = { __typename?: 'Mutation' } & Pick<
-    Mutation,
-    'startSession'
->;
+export type SessionQuery = (
+  { __typename?: 'Query' }
+  & Pick<Query, 'session'>
+);
 
-export type LogoutMutationVariables = Exact<{ [key: string]: never }>;
+export type LoginMutationVariables = Exact<{ [key: string]: never; }>;
 
-export type LogoutMutation = { __typename?: 'Mutation' } & Pick<
-    Mutation,
-    'endSession'
->;
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'startSession'>
+);
+
+export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type LogoutMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'endSession'>
+);
+
 
 export const UserDocument = gql`
     query user($id: ID!) {
-        user(id: $id) {
-            firstName
-            lastName
-            email
-            password
-        }
-    }
-`;
+  user(id: $id) {
+    firstName
+    lastName
+    email
+    password
+  }
+}
+    `;
 
 /**
  * __useUserQuery__
@@ -124,32 +127,22 @@ export const UserDocument = gql`
  *   },
  * });
  */
-export function useUserQuery(
-    baseOptions: Apollo.QueryHookOptions<UserQuery, UserQueryVariables>,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useQuery<UserQuery, UserQueryVariables>(
-        UserDocument,
-        options,
-    );
-}
-export function useUserLazyQuery(
-    baseOptions?: Apollo.LazyQueryHookOptions<UserQuery, UserQueryVariables>,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useLazyQuery<UserQuery, UserQueryVariables>(
-        UserDocument,
-        options,
-    );
-}
+export function useUserQuery(baseOptions: Apollo.QueryHookOptions<UserQuery, UserQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+      }
+export function useUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UserQuery, UserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+        }
 export type UserQueryHookResult = ReturnType<typeof useUserQuery>;
 export type UserLazyQueryHookResult = ReturnType<typeof useUserLazyQuery>;
 export type UserQueryResult = Apollo.QueryResult<UserQuery, UserQueryVariables>;
 export const SessionDocument = gql`
     query session {
-        session
-    }
-`;
+  session
+}
+    `;
 
 /**
  * __useSessionQuery__
@@ -166,42 +159,23 @@ export const SessionDocument = gql`
  *   },
  * });
  */
-export function useSessionQuery(
-    baseOptions?: Apollo.QueryHookOptions<SessionQuery, SessionQueryVariables>,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useQuery<SessionQuery, SessionQueryVariables>(
-        SessionDocument,
-        options,
-    );
-}
-export function useSessionLazyQuery(
-    baseOptions?: Apollo.LazyQueryHookOptions<
-        SessionQuery,
-        SessionQueryVariables
-    >,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useLazyQuery<SessionQuery, SessionQueryVariables>(
-        SessionDocument,
-        options,
-    );
-}
+export function useSessionQuery(baseOptions?: Apollo.QueryHookOptions<SessionQuery, SessionQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<SessionQuery, SessionQueryVariables>(SessionDocument, options);
+      }
+export function useSessionLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<SessionQuery, SessionQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<SessionQuery, SessionQueryVariables>(SessionDocument, options);
+        }
 export type SessionQueryHookResult = ReturnType<typeof useSessionQuery>;
 export type SessionLazyQueryHookResult = ReturnType<typeof useSessionLazyQuery>;
-export type SessionQueryResult = Apollo.QueryResult<
-    SessionQuery,
-    SessionQueryVariables
->;
+export type SessionQueryResult = Apollo.QueryResult<SessionQuery, SessionQueryVariables>;
 export const LoginDocument = gql`
     mutation login {
-        startSession
-    }
-`;
-export type LoginMutationFn = Apollo.MutationFunction<
-    LoginMutation,
-    LoginMutationVariables
->;
+  startSession
+}
+    `;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
 /**
  * __useLoginMutation__
@@ -219,33 +193,19 @@ export type LoginMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useLoginMutation(
-    baseOptions?: Apollo.MutationHookOptions<
-        LoginMutation,
-        LoginMutationVariables
-    >,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useMutation<LoginMutation, LoginMutationVariables>(
-        LoginDocument,
-        options,
-    );
-}
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
+      }
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
-export type LoginMutationOptions = Apollo.BaseMutationOptions<
-    LoginMutation,
-    LoginMutationVariables
->;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
 export const LogoutDocument = gql`
     mutation logout {
-        endSession
-    }
-`;
-export type LogoutMutationFn = Apollo.MutationFunction<
-    LogoutMutation,
-    LogoutMutationVariables
->;
+  endSession
+}
+    `;
+export type LogoutMutationFn = Apollo.MutationFunction<LogoutMutation, LogoutMutationVariables>;
 
 /**
  * __useLogoutMutation__
@@ -263,73 +223,42 @@ export type LogoutMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useLogoutMutation(
-    baseOptions?: Apollo.MutationHookOptions<
-        LogoutMutation,
-        LogoutMutationVariables
-    >,
-) {
-    const options = { ...defaultOptions, ...baseOptions };
-    return Apollo.useMutation<LogoutMutation, LogoutMutationVariables>(
-        LogoutDocument,
-        options,
-    );
-}
+export function useLogoutMutation(baseOptions?: Apollo.MutationHookOptions<LogoutMutation, LogoutMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument, options);
+      }
 export type LogoutMutationHookResult = ReturnType<typeof useLogoutMutation>;
 export type LogoutMutationResult = Apollo.MutationResult<LogoutMutation>;
-export type LogoutMutationOptions = Apollo.BaseMutationOptions<
-    LogoutMutation,
-    LogoutMutationVariables
->;
+export type LogoutMutationOptions = Apollo.BaseMutationOptions<LogoutMutation, LogoutMutationVariables>;
 export type QueryKeySpecifier = ('user' | 'session' | QueryKeySpecifier)[];
 export type QueryFieldPolicy = {
-    user?: FieldPolicy<any> | FieldReadFunction<any>;
-    session?: FieldPolicy<any> | FieldReadFunction<any>;
+	user?: FieldPolicy<any> | FieldReadFunction<any>,
+	session?: FieldPolicy<any> | FieldReadFunction<any>
 };
-export type MutationKeySpecifier = (
-    | 'createUser'
-    | 'startSession'
-    | 'endSession'
-    | MutationKeySpecifier
-)[];
+export type MutationKeySpecifier = ('createUser' | 'startSession' | 'endSession' | MutationKeySpecifier)[];
 export type MutationFieldPolicy = {
-    createUser?: FieldPolicy<any> | FieldReadFunction<any>;
-    startSession?: FieldPolicy<any> | FieldReadFunction<any>;
-    endSession?: FieldPolicy<any> | FieldReadFunction<any>;
+	createUser?: FieldPolicy<any> | FieldReadFunction<any>,
+	startSession?: FieldPolicy<any> | FieldReadFunction<any>,
+	endSession?: FieldPolicy<any> | FieldReadFunction<any>
 };
-export type UserKeySpecifier = (
-    | 'firstName'
-    | 'lastName'
-    | 'email'
-    | 'password'
-    | UserKeySpecifier
-)[];
+export type UserKeySpecifier = ('firstName' | 'lastName' | 'email' | 'password' | UserKeySpecifier)[];
 export type UserFieldPolicy = {
-    firstName?: FieldPolicy<any> | FieldReadFunction<any>;
-    lastName?: FieldPolicy<any> | FieldReadFunction<any>;
-    email?: FieldPolicy<any> | FieldReadFunction<any>;
-    password?: FieldPolicy<any> | FieldReadFunction<any>;
+	firstName?: FieldPolicy<any> | FieldReadFunction<any>,
+	lastName?: FieldPolicy<any> | FieldReadFunction<any>,
+	email?: FieldPolicy<any> | FieldReadFunction<any>,
+	password?: FieldPolicy<any> | FieldReadFunction<any>
 };
 export type TypedTypePolicies = TypePolicies & {
-    Query?: Omit<TypePolicy, 'fields' | 'keyFields'> & {
-        keyFields?:
-            | false
-            | QueryKeySpecifier
-            | (() => undefined | QueryKeySpecifier);
-        fields?: QueryFieldPolicy;
-    };
-    Mutation?: Omit<TypePolicy, 'fields' | 'keyFields'> & {
-        keyFields?:
-            | false
-            | MutationKeySpecifier
-            | (() => undefined | MutationKeySpecifier);
-        fields?: MutationFieldPolicy;
-    };
-    User?: Omit<TypePolicy, 'fields' | 'keyFields'> & {
-        keyFields?:
-            | false
-            | UserKeySpecifier
-            | (() => undefined | UserKeySpecifier);
-        fields?: UserFieldPolicy;
-    };
+	Query?: Omit<TypePolicy, "fields" | "keyFields"> & {
+		keyFields?: false | QueryKeySpecifier | (() => undefined | QueryKeySpecifier),
+		fields?: QueryFieldPolicy,
+	},
+	Mutation?: Omit<TypePolicy, "fields" | "keyFields"> & {
+		keyFields?: false | MutationKeySpecifier | (() => undefined | MutationKeySpecifier),
+		fields?: MutationFieldPolicy,
+	},
+	User?: Omit<TypePolicy, "fields" | "keyFields"> & {
+		keyFields?: false | UserKeySpecifier | (() => undefined | UserKeySpecifier),
+		fields?: UserFieldPolicy,
+	}
 };
--- src/graphql/ssr-types.tsx
+++ src/graphql/ssr-types.tsx
@@ -1,86 +1,65 @@
 import * as Types from './operation-types';
 
 import * as Operations from './operation-types';
-import { NextRouter, useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router'
 import { QueryHookOptions, useQuery } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 import type React from 'react';
-import { getApolloClient } from '../lib';
-export async function getServerPageUser(
-    options: Omit<Apollo.QueryOptions<Types.UserQueryVariables>, 'query'>,
-    ctx?: any,
-) {
-    const apolloClient = getApolloClient(ctx);
+import { getApolloClient} from '../lib';
+export async function getServerPageUser
+    (options: Omit<Apollo.QueryOptions<Types.UserQueryVariables>, 'query'>, ctx? :any ){
+        const apolloClient = getApolloClient(ctx);
+        
+        const data = await apolloClient.query<Types.UserQuery>({ ...options, query: Operations.UserDocument });
+        
+        const apolloState = apolloClient.cache.extract();
 
-    const data = await apolloClient.query<Types.UserQuery>({
-        ...options,
-        query: Operations.UserDocument,
-    });
-
-    const apolloState = apolloClient.cache.extract();
-
-    return {
-        props: {
-            apolloState,
-            data: data?.data,
-            error: data?.error ?? data?.errors ?? null,
-        },
-    };
-}
+        return {
+            props: {
+                apolloState,
+                data: data?.data,
+                error: data?.error ?? data?.errors ?? null,
+            },
+        };
+      }
 export const useUser = (
-    optionsFunc?: (
-        router: NextRouter,
-    ) => QueryHookOptions<Types.UserQuery, Types.UserQueryVariables>,
-) => {
-    const router = useRouter();
-    const options = optionsFunc ? optionsFunc(router) : {};
-    return useQuery(Operations.UserDocument, options);
+  optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.UserQuery, Types.UserQueryVariables>) => {
+  const router = useRouter();
+  const options = optionsFunc ? optionsFunc(router) : {};
+  return useQuery(Operations.UserDocument, options);
 };
-export type PageUserComp = React.FC<{
-    data?: Types.UserQuery;
-    error?: Apollo.ApolloError;
-}>;
+export type PageUserComp = React.FC<{data?: Types.UserQuery, error?: Apollo.ApolloError}>;
 export const ssrUser = {
-    getServerPage: getServerPageUser,
+      getServerPage: getServerPageUser,
+      
+      usePage: useUser,
+    }
+export async function getServerPageSession
+    (options: Omit<Apollo.QueryOptions<Types.SessionQueryVariables>, 'query'>, ctx? :any ){
+        const apolloClient = getApolloClient(ctx);
+        
+        const data = await apolloClient.query<Types.SessionQuery>({ ...options, query: Operations.SessionDocument });
+        
+        const apolloState = apolloClient.cache.extract();
 
-    usePage: useUser,
-};
-export async function getServerPageSession(
-    options: Omit<Apollo.QueryOptions<Types.SessionQueryVariables>, 'query'>,
-    ctx?: any,
-) {
-    const apolloClient = getApolloClient(ctx);
-
-    const data = await apolloClient.query<Types.SessionQuery>({
-        ...options,
-        query: Operations.SessionDocument,
-    });
-
-    const apolloState = apolloClient.cache.extract();
-
-    return {
-        props: {
-            apolloState,
-            data: data?.data,
-            error: data?.error ?? data?.errors ?? null,
-        },
-    };
-}
+        return {
+            props: {
+                apolloState,
+                data: data?.data,
+                error: data?.error ?? data?.errors ?? null,
+            },
+        };
+      }
 export const useSession = (
-    optionsFunc?: (
-        router: NextRouter,
-    ) => QueryHookOptions<Types.SessionQuery, Types.SessionQueryVariables>,
-) => {
-    const router = useRouter();
-    const options = optionsFunc ? optionsFunc(router) : {};
-    return useQuery(Operations.SessionDocument, options);
+  optionsFunc?: (router: NextRouter)=> QueryHookOptions<Types.SessionQuery, Types.SessionQueryVariables>) => {
+  const router = useRouter();
+  const options = optionsFunc ? optionsFunc(router) : {};
+  return useQuery(Operations.SessionDocument, options);
 };
-export type PageSessionComp = React.FC<{
-    data?: Types.SessionQuery;
-    error?: Apollo.ApolloError;
-}>;
+export type PageSessionComp = React.FC<{data?: Types.SessionQuery, error?: Apollo.ApolloError}>;
 export const ssrSession = {
-    getServerPage: getServerPageSession,
+      getServerPage: getServerPageSession,
+      
+      usePage: useSession,
+    }
 
-    usePage: useSession,
-};
